7df0c7d86f399031723f31262fd6413fed19785e
AMHollrah <abarton@umw.edu>
2023-05-22 16:10:09 -0400

    Heart Shape 1
    Creating Basic Heart

--- week2/2.1.emoji/sketch.js
+++ week2/2.1.emoji/sketch.js
@@ -3,5 +3,30 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  Background(220);
+  size(300, 300);
+
+  fill(255, 0, 0); 
+
+//left half of heart
+  beginShape();
+  curveVertex(150, 600);
+  curveVertex(150, 270);
+  curveVertex(50, 150);
+  curveVertex(75, 75);
+  curveVertex(150, 100);
+  curveVertex(150, 300);
+  endShape();
+
+//right half of heart
+  beginShape();
+  curveVertex(150, 300);
+  curveVertex(150, 100);
+  curveVertex(220, 75);
+  curveVertex(250, 150);
+  curveVertex(150, 270);
+  curveVertex(150, 600);
+  endShape();
+ 
+
 }
