9d48a2ba7741fa1fb87bd7af3531c39e62455eec
AMHollrah <abarton@umw.edu>
2023-05-23 16:10:59 -0400

    Circles Exercise
    Changed the colors, height and width

--- week2/2.2.circles/sketch.js
+++ week2/2.2.circles/sketch.js
@@ -1,7 +1,20 @@
 function setup() {
   createCanvas(1200, 1200);
+  noLoop()
 }
 
 function draw() {
   background(220);
+  translate(100,100);
+
+  for (let x = 0; x < 8; x++)
+    for (let y = 0; y < 8; y++){
+      push();
+      translate(x * 100, y * 100);
+      stroke(255,165,0);
+      strokeWeight(8);
+      fill(75,0,130); 
+      ellipse(0,0,150,125);
+      pop();
+    }   
 }
