90847c7f0de87ae522cf048cd251db60085f53d4
AMHollrah <abarton@umw.edu>
2023-05-24 20:09:59 -0400

    Quilt Pattern Squares
    Update using If and Else

--- week2/2.4.quilt-pattern/sketch.js
+++ week2/2.4.quilt-pattern/sketch.js
@@ -1,7 +1,43 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1000, 1000);
+  noLoop()
 }
 
 function draw() {
-  background(220);
+    
+//  border
+  fill(125,0,100);
+  noStroke();
+  rect(0, 0, 1000, 50);// top
+  rect(0, 950, 1000, 52);  // bottom
+  
+  fill(125,0,100);
+  rect(0, 50, 51, 900); // left
+  rect(949, 50, 51, 900); // right
+  
+//background
+  stroke (0,0,0)
+  fill(120,50,100);
+  rect(50, 50, 900, 900);
+  
+  
+  translate(50, 50);
+
+// squares
+  for (let x = 0; x < 9; x++)
+    for (let y = 0; y < 9; y++){
+      push();
+      translate(x * 100, y * 100);
+      
+        if (x == 0,8) {
+          fill(0, 150, 150);
+        } else {
+          fill(120, 50, 100);
+        }
+      
+      rotate(PI / 4.0);
+      rect(36, -35, 70, 70);
+      
+      pop();
+    }  
 }
