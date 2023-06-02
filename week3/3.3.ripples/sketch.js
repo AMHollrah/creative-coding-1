ff35234f2d7a90e99431a683edf7831dafad7909
AMHollrah <abarton@umw.edu>
2023-06-01 20:37:19 -0400

    Ripple Exercise
    updating notes, background and code

--- week3/3.3.ripples/sketch.js
+++ week3/3.3.ripples/sketch.js
@@ -1,7 +1,55 @@
+let Pool;
+let ripple_x = 0;
+let ripple_y = 0;
+let ripple_d = 50;
+let ellipseColor = 0;
+
+let ripplers = [];
+
+function preload() {
+  Pool= img = loadImage('https://amhollrah.github.io/creative-coding-1/week3/3.3.ripples/Pool.jpg');
+}
+
+class Rippler{
+  constructor (x,y){
+    this.x = x,
+    this.y = y,
+    this.d = 0
+  }
+  
+  draw(){
+    this.d += 3;
+    ellipse(this.x, this.y, this.d);
+  }
+}
+
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(1200, 800);
+  ellipseColor = random(255);
 }
 
 function draw() {
-  background(220);
-}
+    background(Pool);
+    stroke (ellipseColor);
+    strokeWeight(4);
+    noFill();
+    
+    
+    for (let i = 0; i < ripplers.length; i++){
+      ripplers[i].draw();
+    }
+    
+  }  
+    
+function mousePressed (){
+    
+    ripple_x = mouseX;
+    ripple_y = mouseY;
+    ripple_d = 0;
+    
+    ripplers.push(new Rippler(mouseX, mouseY));
+    
+  if (mouseIsPressed) {
+        ellipseColor = color(random(255), random(255), random(255));
+    }
+  }
