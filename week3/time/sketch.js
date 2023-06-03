00c897dd61dbcdacac4e854e5ab0bf30ed5fbed3
AMHollrah <abarton@umw.edu>
2023-06-03 17:08:02 -0400

    Time Draft
    Loading the images and background

--- week3/time/sketch.js
+++ week3/time/sketch.js
@@ -1,7 +1,24 @@
+let Sun1;
+let Sun2;
+let Sun3;
+
+let SunflowerSky;
+
+
+function preload() {
+  Sun1 = img = loadImage('https://amhollrah.github.io/creative-coding-1/week3/time/Sun1.png');
+  Sun2 = img = loadImage('https://amhollrah.github.io/creative-coding-1/week3/time/Sun2.png');
+  Sun3 = img = loadImage('https://amhollrah.github.io/creative-coding-1/week3/time/Sun3.png');
+  SunflowerSky = img = loadImage('https://amhollrah.github.io/creative-coding-1/week3/time/SunflowerSky.png');
+}
+
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(900, 500);
 }
 
 function draw() {
-  background(220);
+  background(SunflowerSky);
+    //image(Corgi, mouseX-50, mouseY-80, 100, 160);
+
+
 }
