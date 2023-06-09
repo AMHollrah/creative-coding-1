78e8a14b0f4937783d6dd13506e9e58bf099a1fd
AMHollrah <abarton@umw.edu>
2023-06-08 21:01:00 -0400

    LoveMarkov
    Adding a different book and updating code

--- week4/4.3.love-markov/sketch.js
+++ week4/4.3.love-markov/sketch.js
@@ -1,7 +1,37 @@
+
+//creating markov model
+let rm= RiTa.markov(5);
+
+//data
+let data = document.getElementById("source").innerText;
+
+//train
+rm.addText(data);
+
 function setup() {
-  createCanvas(800, 800);
+
+  //creating sentences
+  let lines = rm.generate(6);
+
+// find the element to add to
+  let letter = select("#letter"); 
+
+// salutation
+  letter.child( createP("My Dearest Pearl,") );
+
+// loop through the array of lines, adding each as a new p element
+  for (let l = 0; l < lines.length; l++){
+	     let paragraph = createP(lines[l]);
+	      letter.child(paragraph);
+  }
+
+// add a closing
+  letter.child( createP("Thank you kindly,<br>Markov"));
 }
 
-function draw() {
-  background(220);
+function mousePressed(){
+	letter.remove();
+	letter = createDiv();
+	letter.id("letter");
+	setup();
 }
