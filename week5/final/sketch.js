80d497a57610ed848c8b9e6cec8cdca196a17a4a
AMHollrah <abarton@umw.edu>
2023-06-15 22:04:17 -0400

    Final Project 1
    Trying something

--- week5/final/sketch.js
+++ week5/final/sketch.js
@@ -1,7 +1,33 @@
+var inc = 0.01
+var start = 0
+
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(1024, 600);
 }
 
 function draw() {
-  background(220);
+  //  noLoop();
+  
+  background(51);
+  stroke (255);
+  noFill();
+  beginShape();
+  var xoff = start;
+  
+  for (var x = 0; x < width; x++) {
+    stroke (255);
+
+// random (height);
+    var y = noise(xoff) * height;
+    vertex (x, y);
+    
+ //go up for amount   
+    xoff += inc;
+  }
+      
+  endShape();
+  
+  start += inc;
+
+  
 }
