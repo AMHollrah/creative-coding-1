996a01490177a44dc4f1060d66f01ae67ea771dc
AMHollrah <abarton@umw.edu>
2023-06-17 11:27:29 -0400

    Adding Roses
    adding on web

---
+++ week3/bouncerevise/sketch.js
@@ -0,0 +1,34 @@
+let rose;
+
+let x = 100;
+let y = 100;
+
+let xspeed = 2.5;
+let yspeed = -2;
+
+function preload() {
+  rose = img = loadImage('https://amhollrah.github.io/creative-coding-1/week2/pixels/SeattleRose.jpg');
+}
+
+function setup() {
+  createCanvas(600, 500);
+}
+
+function draw() {
+  background(192, 38, 84);
+  image(rose, x, y, 160, 190);
+  
+  
+  // Image movement for edge
+  if (x < 0 || x > width - 160) {
+    xspeed = -xspeed;    
+  }
+
+  if (y < 0 || y > height - 190) {
+    yspeed = -yspeed;    
+  }
+
+    x = x + xspeed;
+    y = y + yspeed;
+  
+}
