0b86f0bcedcd4d2769afcd15ac4b4ea3421fc4fc
AMHollrah <abarton@umw.edu>
2023-06-17 11:16:33 -0400

    Quilt Pattern Revised
    updating statement and code

--- week2/pixelsrevise/sketch.js
+++ week2/pixelsrevise/sketch.js
@@ -1,14 +1,21 @@
-let img;
+let Rose;
+let Rose1;
+let Rose2;
+let Rose3;
 //changing size of the ellipse
 let sampleX = 5;
 let sampleY = 5;
 
 function preload(){
-   img = loadImage('https://amhollrah.github.io/creative-coding-1/week2/pixels/SeattleRose.jpg');
+   Rose = img = loadImage('https://amhollrah.github.io/creative-coding-1/week2/pixelsrevise/SeattleRose.jpg');
+   Rose1 = img = loadImage('https://amhollrah.github.io/creative-coding-1/week2/pixelsrevise/Rose1.jpg');
+   Rose2 = img = loadImage('https://amhollrah.github.io/creative-coding-1/week2/pixelsrevise/Rose2.jpg');
+   Rose3 = img = loadImage('https://amhollrah.github.io/creative-coding-1/week2/pixelsrevise/Rose3.jpg');
 }
 function setup() {
   createCanvas(1015, 1000);
   img.loadPixels();
+ 
 }
 
 function draw() {
@@ -16,38 +23,28 @@ function draw() {
   noStroke();
   //translate (5, 12);
   //top corner of the image and then width & height 
-//  image(img, 40, 35, 445, 450);
+  image(Rose, 40, 35, 445, 450);
 
-  for (let x = 0; x <=img.width; x += sampleX){
-    for(let y = 0; y <=img.height; y+= sampleY){
-      var index = (x + y * img.width) * 4;
-      var r = img.pixels[index + 0];
-      var g = img.pixels[index + 1];
-      var b = img.pixels[index + 2];
+ for (let x = 0; x <=Rose1; x += sampleX){
+  for(let y = 0; y <=Rose1; y+= sampleY){
+  var index = (x + y * img.width) * 4;
+     var r = img.pixels[index + 0];
+     var g = img.pixels[index + 1];
+     var b = img.pixels[index + 2];
   
-      fill(r,g,b);
-      ellipse(x,y,sampleX,sampleY);
+    fill(r,g,b);
+    ellipse(x,y,sampleX,sampleY);
     }
 
   }
+
 //bottom left
-image(img, 40, 515, 445, 450);
-for (let x = 40; x <=img.width; x += sampleX){
-  for(let y = 515; y <=img.height; y+= sampleY){
-    var index = (x + y * img.width) * 4;
-    var r = img.pixels[index + 0];
-    var g = img.pixels[index + 1];
-    var b = img.pixels[index + 2];
+image(Rose1, 40, 515, 445, 450);
 
-    fill(r,g,b);
-    ellipse(x,y,sampleX,sampleY);
-  }
-
-}
 //top right
-// image(img, 525, 35, 445, 450);
+// image(Rose2, 525, 35, 445, 450);
 //bottom right
- // image(img, 525, 515, 445, 450);
+ // image(Rose3, 525, 515, 445, 450);
   
 
 
